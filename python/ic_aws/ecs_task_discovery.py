2a706e245430f4656a922ec48767d5a600f57599
Mikko Kaasinen <mikko.kaasinen@digia.com>
2019-02-28 14:39:07 +0000

    ecs_task_discovery added privateIP resolution

--- python/ic_aws/ecs_task_discovery.py
+++ python/ic_aws/ecs_task_discovery.py
@@ -6,14 +6,17 @@ import json
 
 
 class ECSTaskDiscovery(object):
-    def __init__(self, aws_client):
+    def __init__(self, aws_client, ec2_client):
         self._client = aws_client.client()
+        self._ec2_client = ec2_client.client()
 
     def find_tasks(self, cluster_name):
 
         tasks = self._client.list_tasks(
             cluster=cluster_name
         )
+        print json.dumps(tasks)
+
         task_descs_result = self._client.describe_tasks(
             cluster=cluster_name,
             tasks=tasks["taskArns"]
@@ -35,14 +38,36 @@ class ECSTaskDiscovery(object):
 
         return task_descs
 
-    def find_network_bindings(self, task, container_port):
+    def find_network_bindings(self, task, container_port, cluster_name):
         network_bindings = []
         for container in task["containers"]:
-            for network_binding in containers["networkBindings"]:
-                if container_port == network_binding["containerPort"]:
-                    network_bindings.append(network_binding)
-
+            instances = self.find_instances(cluster_name, container["containerArn"])
+            for network_binding in container["networkBindings"]:
+                if container_port == str(network_binding["containerPort"]):
+                    for instance in instances:
+                        network_bindings.append([network_binding, instance["PrivateIpAddress"]])
         return network_bindings
+
+    def find_instances(self, cluster_name, container_arn):
+        container_instances = self._client.describe_container_instances(
+            cluster=cluster_name,
+            containerInstances=[container_arn]
+        )
+
+        instance_ids = []
+        for instance in container_instances["containerInstances"]:
+             instance_ids.append(instance["ec2InstanceId"])
+
+        instances = self._ec2_client.describe_instances(
+            instanceIds=instance_ids
+        )
+
+        result = []
+        for reservation in instances["Reservations"]:
+            for instance in reservation["Instances"]:
+                result.append(instance)
+
+        return result
 
 
 def main(args=None):
@@ -55,16 +80,17 @@ def main(args=None):
     args = parser.parse_args(args)
 
     aws_client = AWSClient("ecs", args)
-    client = ECSTaskDiscovery(aws_client)
+    ec2_client = AWSClient("ec2", args)
+    client = ECSTaskDiscovery(aws_client, ec2_client)
     task_descs = client.find_tasks(args.cluster_name)
 
     names = []
     for task in task_descs:
-        for network_binding in client.find_network_bindings(task, args.container_port):
+        for network_binding in client.find_network_bindings(task, args.container_port, args.cluster_name):
             names.append({
                 "{#TASKARN}": task["taskArn"],
-                "{#BINDIP}": network_binding["bindIP"],
-                "{#HOSTPORT}": network_binding["hostPort"]
+                "{#BINDIP}": network_binding[1],
+                "{#HOSTPORT}": network_binding[0]["hostPort"]
             })
     discovery = {"data": names}
     print json.dumps(discovery)
