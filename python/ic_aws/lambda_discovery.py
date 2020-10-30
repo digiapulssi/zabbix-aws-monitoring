f185d79a1ad18835fb5e1c98e0c8991209b46a1c
Antti-Pekka Meronen <antti-pekka.meronen@digia.com>
2020-10-30 08:45:59 +0000

    Fixes that checkstyle reported.

--- python/ic_aws/lambda_discovery.py
+++ python/ic_aws/lambda_discovery.py
@@ -6,44 +6,46 @@ import json
 
 
 class LambdaDiscovery(object):
+
     def __init__(self, aws_client):
         self._client = aws_client.client()
-        
+
     def get_functions(self, pagination_token=None):
-        
+
         if pagination_token:
             result = self._client.list_functions(Marker=pagination_token)
         else:
             result = self._client.list_functions()
 
         return result
-    
-    
+
+
 def main(args=None):
     parser = ArgumentParser(description="Discover Lambda functions")
     add_aws_client_arguments(parser)
-    
+
     args = parser.parse_args(args)
-    
+
     aws_client = AWSClient("lambda", args)
     client = LambdaDiscovery(aws_client)
-    
+
     result = client.get_functions()
     functions = result['Functions']
 
     # Max function return count is 50
     # if n(functions)>50 pagnation token "NextMarker" will be returned
     while 'NextMarker' in result:
-        result = client.get_functions(pagination_token = result['NextMarker'])
+        result = client.get_functions(pagination_token=result['NextMarker'])
         functions.extend(result['Functions'])
 
     discover_functions = []
     for function in functions:
         discover_functions.append({'{#FUNCTIONNAME}': function['FunctionName'],
                                    '{#DESCRIPTION}': function['Description']})
-        
+
     discovery = {'data': discover_functions}
     print(json.dumps(discovery))
-    
+
+
 if __name__ == "__main__":
     main()
