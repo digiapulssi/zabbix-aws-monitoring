6694f4f4c3272bfe387fccb70015d0bfa0b932f6
Antti-Pekka Meronen <bulkware@users.noreply.github.com>
2024-03-28 07:00:37 +0200

    Version 1.0.13 (#20)

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="aws-monitoring",
-    version="1.0.12",
+    version="1.0.13",
     author="Sami Pajunen",
     author_email="sami.pajunen@digia.com",
     description="Monitoring scripts for AWS services",
